60003d50f5ea077b4c01954814cfe531a0a1959e
lishiwei <lishiwei@oneapm.com>
2017-03-14 11:47:49 +0800

    reformat code

--- src/main/java/com/lishiwei/springboottest/controller/HelloController.java
+++ src/main/java/com/lishiwei/springboottest/controller/HelloController.java
@@ -1,7 +1,7 @@
 package com.lishiwei.springboottest.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
